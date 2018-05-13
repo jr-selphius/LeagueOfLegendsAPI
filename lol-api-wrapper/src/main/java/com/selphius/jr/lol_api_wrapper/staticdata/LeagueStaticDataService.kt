3e05678383de7805f66630c8e830d9354e48c4a2
jr-selphius <sanrafja@hotmail.com>
2018-05-13 15:38:43 +0200

    Static data calls completed.

--- lol-api-wrapper/src/main/java/com/selphius/jr/lol_api_wrapper/staticdata/LeagueStaticDataService.kt
+++ lol-api-wrapper/src/main/java/com/selphius/jr/lol_api_wrapper/staticdata/LeagueStaticDataService.kt
@@ -15,7 +15,7 @@ interface LeagueStaticDataService {
     fun getChampions(@Query("tags") tags: Set<String>,
                      @Query("locale") locale: String,
                      @Query("version") version: String,
-                     @Query("runeData") runeData: Set<String>)
+                     @Query("champListData") champListData: Set<String>)
             : Call<ChampionListDto>
 
     /**
@@ -23,12 +23,201 @@ interface LeagueStaticDataService {
      *
      */
     @GET("/lol/static-data/v3/champions/{id}")
-    fun getChampions(@Path("id") id: Long,
+    fun getChampion(@Path("id") id: Long,
                      @Query("tags") tags: Set<String>,
                      @Query("locale") locale: String,
                      @Query("version") version: String,
-                     @Query("runeData") runeData: Set<String>) : Call<ChampionDto>
+                     @Query("champData") champData: Set<String>)
+            : Call<ChampionDto>
+
+    /**
+     * Retrieves item list
+     *
+     */
+    @GET("/lol/static-data/v3/items")
+    fun getItems(@Query("tags") tags: Set<String>,
+                     @Query("locale") locale: String,
+                     @Query("version") version: String,
+                     @Query("itemListData") itemListData: Set<String>)
+            : Call<ItemListDto>
 
 
+    /**
+     * Retrieves item by ID
+     *
+     */
+    @GET("/lol/static-data/v3/items/{id}")
+    fun getItem(@Path("id") id: Long,
+                    @Query("tags") tags: Set<String>,
+                    @Query("locale") locale: String,
+                    @Query("version") version: String,
+                    @Query("itemData") itemData: Set<String>)
+            : Call<ItemDto>
+
+
+    /**
+     * Retrieves language strings data
+     *
+     */
+    @GET("/lol/static-data/v3/language-strings")
+    fun getLanguageStrings(@Query("locale") locale: String,
+                     @Query("version") version: String)
+            : Call<LanguageStringsDto>
+
+
+    /**
+     * Retrieves supported languages data
+     *
+     */
+    @GET("/lol/static-data/v3/languages")
+    fun getLanguages() : Call<LanguageStringsDto>
+
+
+    /**
+     * Retrieves map data
+     *
+     */
+    @GET("/lol/static-data/v3/maps")
+    fun getMapData() : Call<MapDataDto>
+
+
+    /**
+     * Retrieves mastery list
+     *
+     */
+    @GET("/lol/static-data/v3/masteries")
+    fun getMasteries(@Query("tags") tags: Set<String>,
+                 @Query("locale") locale: String,
+                 @Query("version") version: String,
+                 @Query("masteryListData") masteryListData: Set<String>)
+            : Call<MasteryListDto>
+
+    /**
+     * Retrieves mastery item by ID
+     *
+     */
+    @GET("/lol/static-data/v3/masteries/{id}")
+    fun getMastery(@Path("id") id: Long,
+                     @Query("tags") tags: Set<String>,
+                     @Query("locale") locale: String,
+                     @Query("version") version: String,
+                     @Query("masteryData") masteryData: Set<String>)
+            : Call<MasteryDto>
+
+    /**
+     * Retrieves profile icons
+     *
+     */
+    @GET("/lol/static-data/v3/profile-icons")
+    fun getProfileIcons(@Query("locale") locale: String,
+                 @Query("version") version: String)
+            : Call<ProfileIconDataDto>
+
+    /**
+     * Retrieves realm data
+     *
+     */
+    @GET("/lol/static-data/v3/realms")
+    fun getRealms() : Call<RealmDto>
+
+
+    /**
+     * Retrieves reforged rune path list
+     *
+     */
+    @GET("/lol/static-data/v3/reforged-rune-paths")
+    fun getReforgedRunesPathList(@Query("locale") locale: String,
+                                 @Query("version") version: String)
+            : Call<List<ReforgedRunePathDto>>
+
+    /**
+     * Retrieves reforged rune path by ID
+     *
+     */
+    @GET("/lol/static-data/v3/reforged-rune-paths/{id}")
+    fun getReforgedRunesPath(@Path("id") id: Int,
+                                 @Query("locale") locale: String,
+                                 @Query("version") version: String)
+            : Call<ReforgedRunePathDto>
+
+    /**
+     * Retrieves reforged rune list
+     *
+     */
+    @GET("/lol/static-data/v3/reforged-runes")
+    fun getReforgedRunes(@Query("locale") locale: String,
+                                 @Query("version") version: String)
+            : Call<List<ReforgedRuneDto>>
+
+    /**
+     * Retrieves reforged rune by ID
+     *
+     */
+    @GET("/lol/static-data/v3/reforged-runes/{id}")
+    fun getReforgedRune(@Path("id") id: Int,
+                             @Query("locale") locale: String,
+                             @Query("version") version: String)
+            : Call<ReforgedRuneDto>
+
+    /**
+     * Retrieves rune list
+     *
+     */
+    @GET("/lol/static-data/v3/runes")
+    fun getRunes(@Query("tags") tags: Set<String>,
+                 @Query("locale") locale: String,
+                 @Query("version") version: String,
+                 @Query("runeListData") runeData: Set<String>)
+            : Call<RuneListDto>
+
+    /**
+     * Retrieves rune by ID
+     *
+     */
+    @GET("/lol/static-data/v3/runes/{id}")
+    fun getRune(@Query("tags") tags: Set<String>,
+                 @Query("locale") locale: String,
+                 @Query("version") version: String,
+                 @Query("runeData") runeData: Set<String>)
+            : Call<RuneDto>
+
+
+    /**
+     * Retrieves summoner spell list
+     *
+     */
+    @GET("/lol/static-data/v3/summoner-spells")
+    fun getSummonerSpellList(@Query("tags") tags: Set<String>,
+                @Query("locale") locale: String,
+                @Query("version") version: String,
+                @Query("spellListData") spellListData: Set<String>)
+            : Call<SummonerSpellListDto>
+
+    /**
+     * Retrieves summoner spell by ID
+     *
+     */
+    @GET("/lol/static-data/v3/summoner-spells/{id}")
+    fun getSummonerSpell(@Path("id") id: Long,
+                             @Query("tags") tags: Set<String>,
+                             @Query("locale") locale: String,
+                             @Query("version") version: String,
+                             @Query("spellData") spellData: Set<String>)
+            : Call<SummonerSpellDto>
+
+    /**
+     * Retrieves full tarball link
+     *
+     */
+    @GET("/lol/static-data/v3/tarball-links")
+    fun getTarballLink(): Call<String>
+
+
+    /**
+     * Retrieves version data
+     *
+     */
+    @GET("/lol/static-data/v3/versions")
+    fun getVersions(): Call<List<String>>
 
 }
--- lol-api-wrapper/src/main/java/com/selphius/jr/lol_api_wrapper/staticdata/models.kt
+++ lol-api-wrapper/src/main/java/com/selphius/jr/lol_api_wrapper/staticdata/models.kt
@@ -25,3 +25,268 @@ data class ChampionDto(val info: InfoDto,
                        val id: Int,
                        val blurb: String,
                        val spells: List<ChampionSpellDto>)
+
+data class ItemListDto(val data: Map<String, ItemDto>,
+                       val version: String,
+                       val tree: List<ItemTreeDto>,
+                       val groups: List<GroupDto>,
+                       val type: String)
+
+data class ItemTreeDto(val header: String,
+                       val tags: List<String>)
+
+data class ItemDto(val gold: GoldDto,
+                   val plaintext: String,
+                   val hideFromAll: Boolean,
+                   val inStore: Boolean,
+                   val into: List<String>,
+                   val id: Int,
+                   val stats: InventoryDataStatsDto,
+                   val colloq: String,
+                   val map: Map<String, Boolean>,
+                   val specialRecipe: Int,
+                   val image: ImageDto,
+                   val description: String,
+                   val tags: List<String>,
+                   val effect: Map<String, String>,
+                   val requiredChampion: String,
+                   val from: List<String>,
+                   val group: String,
+                   val consumeOnFull: Boolean,
+                   val name: String,
+                   val consumed: String,
+                   val sanitizedDescription: String,
+                   val depth: Int,
+                   val stacks: Int)
+
+data class GoldDto(val sell: Int,
+                   val total: Int,
+                   val base: Int,
+                   val purchasable: Boolean)
+
+data class InventoryDataStatsDto(val PercentCritDamageMod: Double,
+                                 val PercentSpellBlockMod: Double,
+                                 val PercentHPRegenMod: Double,
+                                 val PercentMovementSpeedMod: Double,
+                                 val FlatSpellBlockMod: Double,
+                                 val FlatCritDamageMod: Double,
+                                 val FlatEnergyPoolMod: Double,
+                                 val PercentLifeStealMod: Double,
+                                 val FlatMPPoolMod: Double,
+                                 val FlatMovementSpeedMod: Double,
+                                 val PercentAttackSpeedMod: Double,
+                                 val FlatBlockMod: Double,
+                                 val PercentBlockMod: Double,
+                                 val FlatEnergyRegenMod: Double,
+                                 val PercentSpellVampMod: Double,
+                                 val FlatMPRegenMod: Double,
+                                 val PercentDodgeMod: Double,
+                                 val FlatAttackSpeedMod: Double,
+                                 val FlatArmorMod: Double,
+                                 val FlatHPRegenMod: Double,
+                                 val PercentMagicDamageMod: Double,
+                                 val PercentMPPoolMod: Double,
+                                 val FlatMagicDamageMod: Double,
+                                 val PercentMPRegenMod: Double,
+                                 val PercentPhysicalDamageMod: Double,
+                                 val FlatPhysicalDamageMod: Double,
+                                 val PercentHPPoolMod: Double,
+                                 val PercentArmorMod: Double,
+                                 val PercentCritChanceMod: Double,
+                                 val PercentEXPBonus: Double,
+                                 val FlatHPPoolMod: Double,
+                                 val FlatCritChanceMod: Double,
+                                 val FlatEXPBonus: Double)
+
+data class GroupDto(val MaxGroupOwnable: String,
+                    val key: String)
+
+data class LanguageStringsDto(val data: Map<String, String>,
+                              val version: String,
+                              val type: String)
+
+data class MapDataDto(val data: Map<String, MapDetailsDto>,
+                      val version: String,
+                      val type: String)
+
+data class MapDetailsDto(val mapName: String,
+                         val image: ImageDto)
+
+data class MasteryListDto(val data: Map<String, MasteryDto>,
+                          val version: String,
+                          val tree: MasteryTreeDto,
+                          val type: String)
+
+data class MasteryTreeDto(val Resolve: List<MasteryTreeListDto>,
+                          val Defense: List<MasteryTreeListDto>,
+                          val Utility: List<MasteryTreeListDto>,
+                          val Offense: List<MasteryTreeListDto>,
+                          val Ferocity: List<MasteryTreeListDto>,
+                          val Cunning: List<MasteryTreeListDto>)
+
+
+data class MasteryTreeListDto(val masteryTreeItems: List<MasteryTreeItemDto>)
+
+data class MasteryTreeItemDto(val masteryId: Int,
+                              val prereq: String)
+
+data class MasteryDto(val prereq: String,
+                      val masteryTree: String,
+                      val name: String,
+                      val ranks: String,
+                      val image: ImageDto,
+                      val sanitizedDescription: List<String>,
+                      val id: Int,
+                      val description: List<String>)
+
+data class ProfileIconDataDto(val data: Map<String, ProfileIconDetailsDto>,
+                              val version: String,
+                              val type: String)
+
+data class ProfileIconDetailsDto(val image: ImageDto,
+                                 val id: Long)
+
+data class RealmDto(val lg: String,
+                    val dd: String,
+                    val l: String,
+                    val n: Map<String, String>,
+                    val profileiconmax: Int,
+                    val store: String,
+                    val v: String,
+                    val cdn: String,
+                    val css: String)
+
+
+data class ReforgedRunePathDto(val slots: List<ReforgedRuneSlotDto>,
+                               val icon: String,
+                               val id: Int,
+                               val key: String,
+                               val name: String)
+
+data class ReforgedRuneSlotDto(val runes: List<ReforgedRuneDto>)
+
+data class ReforgedRuneDto(val runePathName: String,
+                           val runePathId: Int,
+                           val name: String,
+                           val id: Int,
+                           val key: String,
+                           val shortDesc: String,
+                           val longDesc: String,
+                           val icon: String)
+
+data class RuneListDto(val data: Map<String, RuneDto>,
+                       val version: String,
+                       val type: String)
+
+data class RuneDto(val stats: RuneStatsDto,
+                   val name: String,
+                   val tags: List<String>,
+                   val image: ImageDto,
+                   val sanitizedDescription: String,
+                   val rune: MetaDataDto,
+                   val id: Int,
+                   val description: String)
+
+data class RuneStatsDto(val PercentTimeDeadModPerLevel: Double,
+                        val PercentArmorPenetrationModPerLevel: Double,
+                        val PercentCritDamageMod: Double,
+                        val PercentSpellBlockMod: Double,
+                        val PercentHPRegenMod: Double,
+                        val PercentMovementSpeedMod: Double,
+                        val FlatSpellBlockMod: Double,
+                        val FlatEnergyRegenModPerLevel: Double,
+                        val FlatEnergyPoolMod: Double,
+                        val FlatMagicPenetrationModPerLevel: Double,
+                        val PercentLifeStealMod: Double,
+                        val FlatMPPoolMod: Double,
+                        val PercentCooldownMod: Double,
+                        val PercentMagicPenetrationMod: Double,
+                        val FlatArmorPenetrationModPerLevel: Double,
+                        val FlatMovementSpeedMod: Double,
+                        val FlatTimeDeadModPerLevel: Double,
+                        val FlatArmorModPerLevel: Double,
+                        val PercentAttackSpeedMod: Double,
+                        val FlatDodgeModPerLevel: Double,
+                        val PercentMagicDamageMod: Double,
+                        val PercentBlockMod: Double,
+                        val FlatDodgeMod: Double,
+                        val FlatEnergyRegenMod: Double,
+                        val FlatHPModPerLevel: Double,
+                        val PercentAttackSpeedModPerLevel: Double,
+                        val PercentSpellVampMod: Double,
+                        val FlatMPRegenMod: Double,
+                        val PercentHPPoolMod: Double,
+                        val PercentDodgeMod: Double,
+                        val FlatAttackSpeedMod: Double,
+                        val FlatArmorMod: Double,
+                        val FlatMagicDamageModPerLevel: Double,
+                        val FlatHPRegenMod: Double,
+                        val PercentPhysicalDamageMod: Double,
+                        val FlatCritChanceModPerLevel: Double,
+                        val FlatSpellBlockModPerLevel: Double,
+                        val PercentTimeDeadMod: Double,
+                        val FlatBlockMod: Double,
+                        val PercentMPPoolMod: Double,
+                        val FlatMagicDamageMod: Double,
+                        val PercentMPRegenMod: Double,
+                        val PercentMovementSpeedModPerLevel: Double,
+                        val PercentCooldownModPerLevel: Double,
+                        val FlatMPModPerLevel: Double,
+                        val FlatEnergyModPerLevel: Double,
+                        val FlatPhysicalDamageMod: Double,
+                        val FlatHPRegenModPerLevel: Double,
+                        val FlatCritDamageMod: Double,
+                        val PercentArmorMod: Double,
+                        val FlatMagicPenetrationMod: Double,
+                        val PercentCritChanceMod: Double,
+                        val FlatPhysicalDamageModPerLevel: Double,
+                        val PercentArmorPenetrationMod: Double,
+                        val PercentEXPBonus: Double,
+                        val FlatMPRegenModPerLevel: Double,
+                        val PercentMagicPenetrationModPerLevel: Double,
+                        val FlatTimeDeadMod: Double,
+                        val FlatMovementSpeedModPerLevel: Double,
+                        val FlatGoldPer10Mod: Double,
+                        val FlatArmorPenetrationMod: Double,
+                        val FlatCritDamageModPerLevel: Double,
+                        val FlatHPPoolMod: Double,
+                        val FlatCritChanceMod: Double,
+                        val FlatEXPBonus: Double)
+
+data class MetaDataDto(val tier: String,
+                       val type: String,
+                       val isRune: Boolean)
+
+data class SummonerSpellListDto(val data: Map<String, SummonerSpellDto>,
+                                val version: String,
+                                val type: String)
+
+data class SummonerSpellDto(val vars: List<SpellVarsDto>,
+                            val image: ImageDto,
+                            val costBurn: String,
+                            val cooldown: List<Double>,
+                            val effectBurn: List<String>,
+                            val id: Int,
+                            val cooldownBurn: String,
+                            val tooltip: String,
+                            val maxrank: Int,
+                            val rangeBurn: String,
+                            val description: String,
+                            val effect: List<List<List<Double>>>,
+                            val key: String,
+                            val leveltip: LevelTipDto,
+                            val modes: List<String>,
+                            val resource: String,
+                            val name: String,
+                            val costType: String,
+                            val sanitizedDescription: String,
+                            val sanitizedTooltip: String,
+                            val range: String,
+                            val cost: List<Int>,
+                            val summonerLevel: Int)
+
+data class SpellVarsDto(val ranksWith: String,
+                        val dyn: String,
+                        val link: String,
+                        val coeff: List<Double>,
+                        val key: String)
